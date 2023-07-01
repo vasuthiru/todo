1b6cd258bcb2eeb4b416e88c380cd358bf238d8a
vasu <vasu@theimpactworks.com>
2023-07-01 19:31:57 +0530

    delete record added

--- src/app/pages/todo-list/todo-list.component.ts
+++ src/app/pages/todo-list/todo-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
+import { MatTable } from '@angular/material/table';
 import { NavigationExtras, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { DataService } from 'src/app/services/data.service';
@@ -12,16 +14,31 @@ export interface DataSource {
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss'],
 })
-export class TodoListComponent implements OnInit {
+export class TodoListComponent implements OnInit{
   displayedColumns: string[] = ['id', 'name', 'location', 'actions'];
   dataSource!: DataSource[];
-  constructor(private router: Router, private dataService: DataService){}
+  @ViewChild(MatTable) table!: MatTable<any>;
+  horizontalPosition: MatSnackBarHorizontalPosition = 'right';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
+  constructor(private router: Router, private dataService: DataService, private _snackBar: MatSnackBar){}
   ngOnInit(): void {
     this.dataSource = this.dataService.todos;
-    console.log(this.dataSource, 'sdfdsf')
   }
-  editRecord(record: any) {
+  editRecord(record: DataSource) {
     const navigationExtras: NavigationExtras = { state: { record: record } };
     this.router.navigate(['/add-list'],{queryParams: {type:'edit', id: record.id}});
+  }
+  deleteRecord(record:DataSource){
+    this.dataService.deleteRecord(record.id);
+    this.table.renderRows();
+    const msg = 'Record has been deleted!!';
+    this.openSnackBar(msg, 'Success')
+  }
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action, {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+      duration: 5000,
+    });
   }
 }
--- src/app/pages/todo-list/todo-list.module.ts
+++ src/app/pages/todo-list/todo-list.module.ts
@@ -5,7 +5,9 @@ import { TodoListRoutingModule } from './todo-list-routing.module';
 import { TodoListComponent } from './todo-list.component';
 import { MatTableModule } from '@angular/material/table'
 import { MatIconModule } from '@angular/material/icon'
-import { MatTooltipModule } from '@angular/material/tooltip'
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSnackBarModule } from '@angular/material/snack-bar'
+
 @NgModule({
   declarations: [TodoListComponent],
   imports: [
@@ -13,7 +15,8 @@ import { MatTooltipModule } from '@angular/material/tooltip'
     TodoListRoutingModule,
     MatTableModule,
     MatIconModule,
-    MatTooltipModule
+    MatTooltipModule,
+    MatSnackBarModule
   ]
 })
 export class TodoListModule { }
